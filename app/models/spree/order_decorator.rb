7e41bdd12702854b5ebb5153fce6ac5c79e751a4
Zach Karpinski <zkarpinski@gmail.com>
2013-09-06 09:53:57 -0500

    Added short payment summary, fixed credit card equality test

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -12,7 +12,7 @@ Spree::Order.class_eval do
       pa = []
       payments.each do |p|
         pm = p.payment_method
-        if pm.type == "CreditCard"
+        if pm.type == "Credit Card"
            pa.push("#{p.source.cc_type.upcase} #{p.source.display_number}")
         else
           pa.push("#{pm.name.upcase} - #{pm.description}")
@@ -25,5 +25,27 @@ Spree::Order.class_eval do
     end
 
     paysum
+  end
+
+  def short_payment_summary
+    paysum = ""
+    if payments
+      pa = []
+      payments.each do |p|
+        pm = p.payment_method
+        if pm.type == "Credit Card"
+           pa.push("#{p.source.cc_type.upcase}")
+        else
+          pa.push("#{pm.name.upcase}")
+        end
+      end
+
+      paysum = pa.join(", ")
+    else
+      paysum = "N/A"
+    end
+
+    paysum
+
   end
 end
